bb07442adf7a690d7b894474fbf6f12309ba2d54
naviat <haidv.ict@gmail.com>
2019-02-11 16:37:05 +0700

    first upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name = 'dokg',
+    version = '0.1',
+    scripts = ['dokg'],
+    author = 'Hai V.Dam',
+    author_email = 'haidv@tomochain.com',
+    description  = 'My test package',
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    url = 'https://github.com/naviat/dokg',
+    packages = setuptools.find_packages(),
+    classifiers = [
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ],
+)
